bbe18d5387f42250a4271db975529263140cc823
joshy <joshy@EBBY>
2024-02-01 15:49:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1FebbraioEs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============OPERAZIONI==============");
+            Console.WriteLine("Scegli l'operazione da effettuare:");
+            Console.WriteLine("1.: Login");
+            Console.WriteLine("2.: Logout");
+            Console.WriteLine("3.: Verifica data e ora del login");
+            Console.WriteLine("4.: Lista degli accessi");
+            Console.WriteLine("5.: Esci");
+            Console.WriteLine("========================================");
+
+            string scelta =  Console.ReadLine();
+
+            Utente utente1 = new Utente();
+
+            switch (scelta)
+            {
+                case "1":
+                    utente1.Login();
+                    break;
+                    
+            }
+            Console.ReadLine(); 
+        }
+    }
+
+
+}
---
+++ Utente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1FebbraioEs
+{
+    internal class Utente
+    {
+        public string username;
+        public string password;
+        public string confermaPassword;
+        public bool isLoggedIn = false;
+
+        public void Login()
+        {
+            Console.WriteLine("Inserisci username: ");
+            this.username = Console.ReadLine();
+            Console.WriteLine("Inserisci password");
+            this.password = Console.ReadLine();
+            Console.WriteLine("Conferma password");
+            this.confermaPassword = Console.ReadLine();
+
+            if (this.password == this.confermaPassword && this.username != " ")
+            {
+                this.isLoggedIn = true;
+                Console.WriteLine("Login effetuato");
+            }
+            else 
+            {
+                Console.WriteLine("Login non riuscito");
+             }
+        }
+
+        public void Logout()
+        {
+            if (this.isLoggedIn)
+            {
+                Console.WriteLine("Logout effetutato");
+            }
+            else 
+            {
+                Console.WriteLine("ERROR: l'utente non ha fatto il login");
+            }
+        }
+    }
+}
